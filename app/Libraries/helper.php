a4b27e7efd3193ef207ab6ce968c9f8bb7f025d6
van <vgalvoso@gmail.com>
2022-03-05 23:35:45 +0800

    Create Helper class

--- app/Libraries/helper.php
+++ app/Libraries/helper.php
@@ -1,62 +1,53 @@
 <?php
 
-$date = Date("Y-m-d");
-$year = Date("Y");
+class Helper{
+    /**
+     * Generate output in JSON format and terminate the script
+     *
+     * @param string|array $message value to
+     * 
+     * @author Van
+     * 
+     * @return string JSON encoded string
+     */ 
+    public static function output($message){
+        echo $message;
+        exit(0);  
+    }
 
-//error handler function
-function errorHandler($errno, $errstr) {
-    header("Location:error?message=$errstr");
-}
-  
-//set error handler
-//set_error_handler("errorHandler");
+    /**
+     * Convert associative array into indexed array
+     * 
+     * @param array $array associative array
+     * 
+     * @author Van
+     * 
+     * @return array indexed array
+     */ 
+    public static function assocToIndexed($array){
+        return array_map('extractAssocArray', $array);
+    }
 
-/**
- * Generate output in JSON format and terminate the script
- *
- * @param string|array $message value to
- * 
- * @author Van
- * 
- * @return string JSON encoded string
- */ 
-function output($message){
-    echo json_encode($message);
-    exit(0);  
-}
+    /**
+     * Extract value of an associative array
+     * - mainly used as callback
+     * 
+     * @param array $value single item associative array
+     * 
+     * @author Van
+     * 
+     * @return mixed value of associative array
+     */ 
+    public static function extractAssocArray($value){
+        $val = array_values($value);
+        return $val[0];
+    }
 
-/**
- * Convert associative array into indexed array
- * 
- * @param array $array associative array
- * 
- * @author Van
- * 
- * @return array indexed array
- */ 
-function assocToIndexed($array){
-    return array_map('extractAssocArray', $array);
-}
+    public static function download($fileName,$html,$extension){
+        // Headers for download 
+        header("Content-Type: application/$extension"); 
+        header("Content-Disposition: attachment; filename=$fileName.$extension"); 
 
-/**
- * Extract value of an associative array
- * - mainly used as callback
- * 
- * @param array $value single item associative array
- * 
- * @author Van
- * 
- * @return mixed value of associative array
- */ 
-function extractAssocArray($value){
-    $val = array_values($value);
-    return $val[0];
-}
-
-function download($fileName,$html,$extension){
-    // Headers for download 
-    header("Content-Type: application/$extension"); 
-    header("Content-Disposition: attachment; filename=$fileName.$extension"); 
-
-    echo $html;
+        echo $html;
+    }
 }
